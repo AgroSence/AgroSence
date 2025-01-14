747ea8a02eb899b30b00863cbc4d522a333bb9b8
Dhruv <sheredhruv@gmail.com>
2025-01-14 23:29:05 +0530

    Updating the Minor changes for responsiveness

--- agrosence/components/contact/ContactHero.jsx
+++ agrosence/components/contact/ContactHero.jsx
@@ -10,6 +10,10 @@ const ContactHero = () => {
         height: "400px",
       }}
     >
+      <div
+        className="position-absolute top-0 start-0 w-100 h-100"
+        style={{ backgroundColor: "rgba(0,0,0,0.4)" }}
+      ></div>
       <h1 className="text-white position-relative display-2 fw-bold">
         Contact us
       </h1>
--- agrosence/components/home/HomeHeader.jsx
+++ agrosence/components/home/HomeHeader.jsx
@@ -3,23 +3,40 @@ import { resource } from "../../resource";
 
 const Header = () => {
   const headerStyle = {
-    backgroundImage: `url(${resource.WelcomeHomeImg.src})`, // Replace with your image URL
+    backgroundImage: `url(${resource.WelcomeHomeImg.src})`,
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    height: "400px", // Adjust as needed
+    height: "400px",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     position: "relative",
     color: "white",
     textAlign: "center",
+    overflow: "hidden",
+  };
+
+  const overlayStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.4)", // Dark overlay
+    zIndex: 1,
+  };
+
+  const textStyle = {
+    position: "relative",
+    zIndex: 2, // Places text above the overlay
   };
 
   return (
-    <header style={headerStyle} className="overflow-x-hidden">
-      <div>
-        <h1>Welcome to the world of Agriculture</h1>
+    <header style={headerStyle}>
+      <div style={overlayStyle}></div> {/* Dark Overlay */}
+      <div style={textStyle}>
+        <h1>Welcome to the World of Agriculture</h1>
       </div>
     </header>
   );
